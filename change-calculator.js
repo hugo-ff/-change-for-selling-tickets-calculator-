ee71cf149f8315476990c0f9a4a4ce95ff10e609
Hugo <hugo.flotts@fizzmod.com>
2021-09-25 04:51:37 -0300

    feat: add ticket function. add validateArray util.

--- change-calculator.js
+++ change-calculator.js
@@ -1,3 +1,34 @@
-const tickets = () => {};
+import { validateArray } from './utils';
 
-export default tickets;
+export const EMPTY_QUEUE_MESSAGE = 'There are no people in the queue.';
+
+export const tickets = peopleInQueue => {
+	if (!validateArray(peopleInQueue)) return EMPTY_QUEUE_MESSAGE;
+
+	const moneyBills = {
+		25: 0,
+		50: 0,
+		100: 0,
+	};
+
+	const moneyOperations = {
+		25: () => (moneyBills[25] += 1),
+		50: () => {
+			moneyBills[25] -= 1;
+			moneyBills[50] += 1;
+		},
+		100: () => {
+			moneyBills[50] ? (moneyBills[50] -= 1) : (moneyBills[25] -= 2);
+			moneyBills[25] -= 1;
+		},
+	};
+
+	const checkIfHasChangeForEachPerson = peopleInQueue.every(mbill => {
+		moneyOperations[mbill]();
+		if (mbill === 25) return true;
+		if (moneyBills[25] < 0 || moneyBills[50] < 0) return false;
+		return true;
+	});
+
+	return checkIfHasChangeForEachPerson ? 'SI' : 'NO';
+};
--- change-calculator.spec.js
+++ change-calculator.spec.js
@@ -1,11 +1,15 @@
-import tickets from './change-calculator';
+import { tickets, EMPTY_QUEUE_MESSAGE } from './change-calculator';
 
 test('calculate if has enough change to sell tickets to each person in the queue', () => {
 	const calculateChange1 = tickets([25, 25, 50]);
-	const calculateChange2 = tickets([25, 100]);
-	const calculateChange3 = tickets([25, 25, 50, 50, 100]);
-
 	expect(calculateChange1).toEqual('SI');
+
+	const calculateChange2 = tickets([25, 100]);
 	expect(calculateChange2).toEqual('NO');
+
+	const calculateChange3 = tickets([25, 25, 50, 50, 100]);
 	expect(calculateChange3).toEqual('NO');
+
+	const calculateChange4 = tickets([]);
+	expect(calculateChange4).toEqual(EMPTY_QUEUE_MESSAGE);
 });
