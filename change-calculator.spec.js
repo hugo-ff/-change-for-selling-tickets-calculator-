53d52f546a8d1543233ca3cdfed40f7ee243f159
Hugo <hugo.flotts@fizzmod.com>
2021-09-25 04:53:18 -0300

    test: add toBe assertion matcher instead of toEqual

--- change-calculator.spec.js
+++ change-calculator.spec.js
@@ -2,14 +2,14 @@ import { tickets, EMPTY_QUEUE_MESSAGE } from './change-calculator';
 
 test('calculate if has enough change to sell tickets to each person in the queue', () => {
 	const calculateChange1 = tickets([25, 25, 50]);
-	expect(calculateChange1).toEqual('SI');
+	expect(calculateChange1).toBe('SI');
 
 	const calculateChange2 = tickets([25, 100]);
-	expect(calculateChange2).toEqual('NO');
+	expect(calculateChange2).toBe('NO');
 
 	const calculateChange3 = tickets([25, 25, 50, 50, 100]);
-	expect(calculateChange3).toEqual('NO');
+	expect(calculateChange3).toBe('NO');
 
 	const calculateChange4 = tickets([]);
-	expect(calculateChange4).toEqual(EMPTY_QUEUE_MESSAGE);
+	expect(calculateChange4).toBe(EMPTY_QUEUE_MESSAGE);
 });
